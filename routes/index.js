c24e004511d2b2b1649885f59611791e60783fa4
WASPEl2 <aekapab10@gmail.com>
2022-11-09 09:19:36 +0700

    last test

--- routes/index.js
+++ routes/index.js
@@ -2,5 +2,7 @@ const express = require("express")
 const router = express.Router()
 
 router.get("/", (req, res) => res.render("welcome"))
+app.get("/index", (req, res) => res.render("welcome"))
+
 
 module.exports = router;
